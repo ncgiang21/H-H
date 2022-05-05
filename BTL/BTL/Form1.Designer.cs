12d9131ec1b815a93c4f94364582678b28bcad2c
ncgiang21 <nguyenconggiang21@gmail.com>
2022-05-06 00:56:56 +0700

    BTL

---
+++ BTL/BTL/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BTL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Name", System.Windows.Forms.HorizontalAlignment.Left);
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.nameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.SystemColors.Highlight;
+            this.start.Location = new System.Drawing.Point(23, 55);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(129, 52);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Red;
+            this.stop.Location = new System.Drawing.Point(23, 277);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(129, 49);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // tb
+            // 
+            this.tb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb.Location = new System.Drawing.Point(23, 169);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(196, 22);
+            this.tb.TabIndex = 2;
+            this.tb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView
+            // 
+            this.listView.BackColor = System.Drawing.SystemColors.Control;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameColumn});
+            listViewGroup1.Header = "Name";
+            listViewGroup1.Name = "Name";
+            this.listView.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            this.listView.Location = new System.Drawing.Point(246, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(542, 426);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.Text = "Name";
+            this.nameColumn.Width = 483;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "BTL HĐH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader nameColumn;
+    }
+}
+
---
+++ BTL/BTL/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BTL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadProcessList();
+        }
+        void loadProcessList()
+        {
+            listView.Items.Clear();
+            Process[] processesList = Process.GetProcesses();
+            foreach (Process process in processesList)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.Tag = process;
+                listView.Items.Add(item);
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            string text = tb.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = text;
+            process.Start();
+            loadProcessList();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView.SelectedItems[0];
+            Process process = (Process)item.Tag;
+            process.Kill();
+            loadProcessList();
+        }
+    }
+}
